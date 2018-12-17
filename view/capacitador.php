ae660dc2e24f63743fc4f1acf29df83ae0c4b704
MisterT <jjolong@miumg.edu.gt>
2018-12-16 22:18:36 -0600

    segundo
    segundo

---
+++ view/capacitador.php
@@ -0,0 +1,81 @@
+<div class="row wow fadeIn">
+    <div class="col-md-12 mb-12">
+        <div class="card mb-12">
+            <div class="card-header text-left">
+                Capacitadores
+            </div>
+                <div class="card-body">
+                    <!-- <canvas id="pieChart"></canvas> -->
+                    <form role="form" class="form-horizontal">
+                        <div class="form-group row">
+                            <label for="dpi" class=" col-sm-2 control-label">DPI:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="dpi" id="dpi" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nombre" class=" col-sm-2 control-label">NOMBRE:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="nombre" id="nombre" class="form-control">
+                            </div>
+
+                            <label for="apellido" class=" col-sm-2 control-label">APELLIDO:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="apellido" id="apellido" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="fecha" class=" col-sm-2 control-label">FECHA NAC:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="fecha" id="fecha" class="form-control">
+                            </div>
+
+                            <label for="edad" class=" col-sm-2 control-label">EDAD:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="edad" id="edad" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="direccion" class=" col-sm-2 control-label">DIRECCIÓN:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="direccion" id="direccion" class="form-control">
+                            </div>
+
+                            <label for="telefono" class=" col-sm-2 control-label">TELÉFONO:</label> 
+                            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                <input type="text" name="telefono" id="telefono" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                                <button type="submit" class="btn btn-primary">enviar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-12 col-md-12 mb-12 tabla">
+        <div class="card">
+            <div class="card-header">Ingresadas</div>
+            <div class="card-body">
+                <table class="table">
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
